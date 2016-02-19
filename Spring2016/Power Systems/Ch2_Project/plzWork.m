f6cee98c7d4d105fb309f11a8511319d7d490bea
caok1 <caok1@tcnj.edu>
2016-02-18 23:09:01 -0500

    Finished matlab function
    * Finished initial function code
    * function needs testing

--- Spring2016/Power Systems/Ch2_Project/plzWork.m
+++ Spring2016/Power Systems/Ch2_Project/plzWork.m
@@ -1,15 +1,6 @@
 function  [I1, I2, IL] = plzWork(V1, Z1, Z2, Z3, ZL)
 
-magZ1 = abs(Z1);
-magZ2 = abs(Z2);
-magZ3 = abs(Z3);
-
-angZ1 = angle(Z1);
-angZ2 = angle(Z2);
-angZ3= angle(Z3);
-
-%I1 = [abs(V1)/magZ1 angle(V1)/angZ1);
-
+%Part 1
 %Solve for I1
 ZT = ((Z3 + ZL) * Z2)/((Z3+ZL)+Z2);
 I1 = V1/(Z1 + ZT);
@@ -36,10 +27,10 @@ IL = [abs(IL) angle(IL)];
 
 %Part 2
 V3 = V2 - (IL * ZL);
-PL = V3 * IL * cos(angle(V3) - angle(IL));
-QL = V3 * IL * sin(angle(IL - V3));
+PL = (V3/sqrt(2)) * (IL/sqrt(2)) * cos(angle(V3) - angle(IL));
+QL = (V3/sqrt(2)) * (IL/sqrt(2)) * sin(angle(IL - V3));
 
-SL = P + Q;
+SL = PL + QL;
 QC = -(QL);
 
 S = V3^2/(-QC);
@@ -50,24 +41,31 @@ XC = S;
 %Part 3
 %Solve for I1
 newZT = (ZL * XC)/ (ZL + XC);
-newZT = ((Z3 + newZT) * Z2)/((Z3+newZT)+Z2);
-newI1 = V1/(Z1 + newZT);
+newZT = ((Z3 + newZT) * Z2)/((Z3 + newZT) + Z2);
+newIL = V1/(Z1 + newZT);
 
 %Solve for V2
-newV2 = V1 - (newI1 * Z1);
+newV2 = V1 - (newIL * Z1);
 
 %Solve for I2
 newI2 = V2/Z2;
 
 %Solve for IL
-newIL = V2/(Z3 + ZL);
+newIL = V2/(Z3 + newZT);
 
 %Solve for apparent Power SL
-newV3 = newV2 - (newIL * newZL);
-PL = V3 * IL * cos(angle(V3) - angle(IL));
-QL = V3 * IL * sin(angle(IL - V3));
+newV3 = newV2 - (newIL * newZT);
+newPL = (newV3/sqrt(2)) * (newIL/sqrt(2)) * cos(angle(newV3) - angle(newIL));
+newQL = (newV3/sqrt(2)) * (newIL/sqrt(2)) * sin(angle(newIL) - angle(newV3));
 
-SL = P + Q;
+newSL = newPL + newQL;
+
+newPLoss = (newI1^2 * Z1) + (newI2^2 * Z2) + (newIL^2 * Z3);
+
+%Part 4
+%Comparing Ratios
+ratio = PLoss/PL;
+newRatio = newPLoss/newPL;
 end
 
 
