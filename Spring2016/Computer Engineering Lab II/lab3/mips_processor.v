02e5fa03c996917da64a93e6dccd95321844cdbd
caok1 <caok1@tcnj.edu>
2016-04-05 23:21:23 -0400

    Update MIPS processor

--- Spring2016/Computer Engineering Lab II/lab3/mips_processor.v
+++ Spring2016/Computer Engineering Lab II/lab3/mips_processor.v
@@ -8,13 +8,13 @@
 
 `timescale 1ns / 1ps
 
-module mips_processor(clk, rst_n, instr, pc_inp, pc_out, wr_dat, wr_reg
+module mips_processor(clk, rst_n, instr, pc_out, wr_dat, wr_reg
     );
 
     input                       clk;
     input                       rst_n;
     
-    input           [31:0]      pc_inp;             // Input PC value
+    //input           [31:0]      pc_inp;             // Input PC value
     input           [31:0]      instr;              // Instruction word
 
     output reg      [31:0]      pc_out;             // Output PC value    
@@ -43,23 +43,6 @@ module mips_processor(clk, rst_n, instr, pc_inp, pc_out, wr_dat, wr_reg
     wire            [31:0]      immed_ext;          //sign extended immediate value
     wire            [31:0]      immed_shift;
     
-    // Reading in instructions
-    always @(posedge clk or negedge rst_n)
-    begin
-        if(!rst_n)
-            pc_reg[0] <= 32'd0;
-        else
-            pc_reg[0] <= pc_inp + 32'd4;
-    end
-    
-    always @(*)
-    begin
-        pc_reg[1]   <= pc_reg[0];
-        if(branch_value)                //need to define branch_value in mem stage
-            pc_out  <= pc_reg[2];
-        else
-            pc_out  <= pc_inp + 32'd4;
-    end
     
     
     
@@ -72,11 +55,35 @@ module mips_processor(clk, rst_n, instr, pc_inp, pc_out, wr_dat, wr_reg
     
 //INSTRUCTION FETCH STEP
 //--------------------------------------------------------------------------------------
+    
+    // clock PC. Reading instruction memory in test bench will not be clocked
+    
 
+
+    always @(posedge clk or negedge rst_n)
+    begin
+        if(!rst_n)
+            pc_out <= 32'd0;
+        else if (PCSrc == 1'b0)     //if select bit is 0
+            pc_out <= pc_reg[0]
+        else                        //if select bit is 1
+            pc_out <= pc_reg[2];
+    end
+    
+    always @(*)
+    begin
+        pc_reg[0] <= pc_out + 32'd4;
+        pc_reg[1] <= pc_reg[0];
+    end
+    
+    
+    //IF/ID Registers
     always @(*)
     begin
         instr_reg <= instr;
     end
+    
+    
     
     
     
@@ -91,36 +98,65 @@ module mips_processor(clk, rst_n, instr, pc_inp, pc_out, wr_dat, wr_reg
 
 //INSTRUCTION DECODE STEP
 //--------------------------------------------------------------------------------------
-    //Register module declaration
-    //control module declaration
 
+    //control module declaration
+    control mips_control(
+        .op_code            (),
+        .RegDst             (),
+        .ALUSrc             (),
+        .MemtoReg           (),
+        .RegWrite           (),
+        .MemRead            (),
+        .MemWrite           (),
+        .Branch             (),
+        .ALUOp1             (),
+        .ALUOp0             ()
+    );
     
     // Reading in the opcode and determining instruction type
-    always @(posedge clk or negedge rst_n)
+    always @(posedge clk)
     begin
-        if(!rst_n)
-        begin
-            op_code     <= 6'd0;
-            rs          <= 5'd0;
-            rt          <= 5'd0;
-            rd          <= 5'd0;
-            shamt       <= 5'd0;
-            funct       <= 6'd0;
-            immediate   <= 15'd0; 
-        end
-        else
-        begin
-            op_code     <= instr_reg[31:26];
-            rs          <= instr_reg[25:21];
-            rt          <= instr_reg[20:16];
-            rd          <= instr_reg[15:11];
-            shamt       <= instr_reg[10: 6];
-            funct       <= instr_reg[ 5: 0];
-            immediate   <= instr_reg[15: 0]; 
-        end
+        op_code     <= instr_reg[31:26];
+        rs          <= instr_reg[25:21];
+        rt          <= instr_reg[20:16];
+        rd          <= instr_reg[15:11];
+        shamt       <= instr_reg[10: 6];
+        funct       <= instr_reg[ 5: 0];
+        immediate   <= instr_reg[15: 0]; 
+
     end    
     
+    
+    //Register module declaration
+    registers mips_registers(
+        .clk                (clk),
+        .rst_n              (rst_n),
+        .rs                 (rs),
+        .rt                 (rt),
+        .wr_reg             (),
+        .wr_dat             (),
+        .read_dat1          (read_data1),
+        .read_dat2          (read_data2)
+    );
+    
+    //sign-extend immediate value
     assign immed_ext = {16{immediate[15]}};
+    
+    
+    
+    //ID/EX Register values
+    
+    always @(*)
+    begin
+        pc_reg[2] <= pc_reg[1];
+    end
+    
+    assign read_data1_reg = read_data1;
+    assign read_data2_reg = read_data2;
+    assign immed_ext_reg = immed_ext;
+    assign rt_reg = rt;
+    assign rd_reg = rd;
+    
 //--------------------------------------------------------------------------------------
     
     
@@ -146,8 +182,24 @@ module mips_processor(clk, rst_n, instr, pc_inp, pc_out, wr_dat, wr_reg
     //assign mux_alu = (ALUSrc == 1'b0) ? read_data2:         //Need to define ALUSrc and read_data2
     //                 immed_ext;
     
-    //ALU declaration
+    
+    
     //ALU control declaration
+    alu_control aluControl(
+        .ALUOp              (),
+        .funct              (),
+        .operation          ()
+    );
+    
+    
+    //ALU declaration
+    alu alu1(
+        .a                  (),
+        .b                  (),
+        .operation          (),
+        .result             (),
+        .zero               ()                       
+    );
     
     
     //Register Destination Mux
